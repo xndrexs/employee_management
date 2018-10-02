04e9adee07836342e1ee27027da3044d98f2a21c
Andreas Pöhler <andreas.poehler@detim.de>
2018-10-02 16:49:40 +0200

    @Data für Abstrakte Klasse

--- backend/src/main/java/de/detim/employeemanagement/models/ModelBase.java
+++ backend/src/main/java/de/detim/employeemanagement/models/ModelBase.java
@@ -1,10 +1,13 @@
 package de.detim.employeemanagement.models;
 
+import lombok.Data;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+@Data
 @MappedSuperclass
 public abstract class ModelBase {
     @Id
