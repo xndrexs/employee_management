0c68c3c2cdfeab54b0573c360174b766ef66013e
Andreas Pöhler <andreas.poehler@detim.de>
2018-10-04 16:21:23 +0200

    Mitarbeiter hinzugefügt,  Komponente für Detail-Seite erstellt

--- frontend/employee-management/src/app/app.module.ts
+++ frontend/employee-management/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule,
--- frontend/employee-management/src/app/employees/employees.ts
+++ frontend/employee-management/src/app/employees/employees.ts
@@ -1,6 +1,8 @@
 import { Employee} from './employee';
 
 export const EMPLOYEES: Employee [] = [
-  { id: 2, name: 'Patrick'},
-  { id: 3, name: 'Amelia'}
+  { id: 1, name: 'Andreas' },
+  { id: 2, name: 'Patrick' },
+  { id: 3, name: 'Amelia' },
+  { id: 4, name: 'Benno'}
 ];
